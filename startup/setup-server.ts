6245915fe735b8b1865a00396fed7bd263d5b51e
CreatorXperience <allyearmustobey@gmail.com>
2023-12-22 19:40:42 +0100

    ch: updated .env file

--- startup/setup-server.ts
+++ startup/setup-server.ts
@@ -5,7 +5,7 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import dotenv from "dotenv";
 dotenv.config();
 
-async function setupServer(port: string | undefined) {
+async function setupServer(port: string | undefined | number) {
   let mongoServer: MongoMemoryServer = await getUri(connectToMongoDB);
 
   return mongoServer;
